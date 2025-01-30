9e17645767feb1530f305d0138116165ba1ae626
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-01-29 20:52:54 -0700

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const minimum = 1;
+const maximum = 50;
+
+const  random = Math.floor(Math.random() * (maximum -minimum + 1)) + minimum;
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
